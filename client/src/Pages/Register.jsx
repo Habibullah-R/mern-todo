dacb62c7204c632f0f449db1967b66d54cffe4aa
Habibullah <habibullahrashid150@gmail.com>
2024-04-11 16:34:07 +0500

    again resolved

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     e.preventDefault();
     try {
       dispatch(signUpStart())
-      const res = await fetch('http://localhost:3000/api/auth/register',{
+      const res = await fetch('/api/auth/register',{
       method:"POST",
       headers:{
         "Content-Type":"application/json"
